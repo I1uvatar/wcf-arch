dd60c0accbc977d4ccabdef229b3cb105625f2c7
i1uvatar <marko@iluvatar.info>
2015-05-25 22:18:36 +0200

    Added web hosting

---
+++ src/Hosting/WebHosting/BookServiceHost/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BookServiceInitializer = BookServiceInitializer.BookServiceInitializer;
+
+
+namespace BookServiceHost
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            global::BookServiceInitializer.BookServiceInitializer initializer =
+                new global::BookServiceInitializer.BookServiceInitializer("BookService");
+
+            initializer.Initialize();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
